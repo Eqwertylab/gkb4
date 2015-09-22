2bc4a58ea16bd550e7454b5a047ee7dd01043345
Eqwertylab <79225555735@ya.ru>
2015-09-22 08:25:48 +0500

    sdfsdfsdf

--- src/js/app.js
+++ src/js/app.js
@@ -34,49 +34,61 @@ App = {
 
     map: function() {
 
-      var myMap, 
-          myPlacemark;
-
       ymaps.ready(init);
 
-      if( '#map' ) {
+      function init() { 
 
-        setMap();
+        if( !$('#map').length ) return;
+
+        var map = setMap();
+
+        $('.contab__link').click(function(event) {
+
+          event.preventDefault();
+          
+          if( $(this).hasClass('contab__link_active') )
+            return;
+
+          $('.contab__item_active').removeClass('contab__item_active');
+          $href = $(this).attr('href');
+          $($href).addClass('contab__item_active');
+
+
+          $('.contab__link_active').removeClass('contab__link_active');
+          $(this).addClass('contab__link_active');
+          map.destroy();
+          map = setMap();
+        });
+
       }
 
       function setMap() {
+        var myMap, 
+            myPlacemark;
 
         var coor = $('.contab__link_active').data('map');
+        var coor_arr = coor.split(',');
+
+        var x = coor_arr[0];
+        var y = coor_arr[1];
+
+        myMap = new ymaps.Map("map", 
+          {
+            center: [x, y],
+            zoom: 16,
+            controls: ["zoomControl", "fullscreenControl"],
+          }
+        ); 
+        myMap.behaviors.disable('scrollZoom');
+
+        myPlacemark = new ymaps.Placemark([x, y], {
+            hintContent: $('.contab__link_active').text(),
+            balloonContent: $('.contab__link_active').text()
+        });
         
-      }
+        myMap.geoObjects.add(myPlacemark);
 
-      $('.contab__link').click(function(event) {
-      
-        event.preventDefault();
-
-        if( !$(this.hasClass('contab__link_active')) ) {
-          var coor = $(this).data('map');
-        }
-
-        var coor = $(this).data('map');
-
-        myMap.destroy();
-
-        alert(coor);
-      });
-
-      function init(x,y) { 
-          myMap = new ymaps.Map("map", {
-              center: [x, y],
-              zoom: 7
-          }); 
-          
-          myPlacemark = new ymaps.Placemark([x, y], {
-              hintContent: 'Москва!',
-              balloonContent: 'Столица России'
-          });
-          
-          myMap.geoObjects.add(myPlacemark);
+        return myMap;
       }
     },
 
