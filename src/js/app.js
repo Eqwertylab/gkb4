995e0bdb6d79cd3bc6474c131b3e186c61021ca9
Eqwertylab <79225555735@ya.ru>
2015-09-25 21:49:16 +0500

    add contact

--- src/js/app.js
+++ src/js/app.js
@@ -166,16 +166,19 @@ App = {
 
     menu: function() {
 
-    	$('.mmenu__item_container > .mmenu__link')
-        .click(function(event) {
+      $( ".menu__inner" ).parent().addClass('menu__item_container');
+      $( ".mmenu__inner" ).parent().addClass('mmenu__item_container');
+
+    	$('.mmenu')
+        .on('click', '.mmenu__item_container > .mmenu__link', function(event) {
           
           event.preventDefault();
           var parent_elem = $(this).parent();
           parent_elem.toggleClass('mmenu__item_open');
         });
 
-      $('.menu__item_container > .menu__link')
-	    	.click(function(event) {
+      $('menu')
+	    	.on('click', '.menu__item_container > .menu__link', function(event) {
 	    		event.preventDefault();
 	    	});
       
