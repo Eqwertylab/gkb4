6afd7a22b29fbb3b472cd8a045972d07a84fb371
dmitriy.karyakin <dmitriy.karyakin@walletone.com>
2017-08-04 10:02:04 +0500

    fix isViewport finction

--- src/chunks/fix-order-link/fix-order-link.js
+++ src/chunks/fix-order-link/fix-order-link.js
@@ -21,10 +21,10 @@
   {
     var rect = element.getBoundingClientRect();
     var html = document.documentElement;
+
     return (
-      rect.top >= 0 &&
+      rect.bottom >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || html.clientHeight) &&
       rect.right <= (window.innerWidth || html.clientWidth)
     );
   }
